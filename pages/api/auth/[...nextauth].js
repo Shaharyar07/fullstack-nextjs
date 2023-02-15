97d5c49f9f593d32a4a99690c3c43ba4ecd42c65
Shaharyar07 <shaharyar.tariq248@gmail.com>
2023-02-15 21:42:37 +0500

    minor change

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../prisma/client";
 
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
